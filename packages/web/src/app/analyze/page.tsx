c233a5be77bfd169d6b837eacf17bcdfd6cdb961
Dean Shub <deanshub@gmail.com>
2023-05-19 08:41:47 +0300

    options from server

--- packages/web/src/app/analyze/page.tsx
+++ packages/web/src/app/analyze/page.tsx
@@ -1,21 +1,23 @@
+import fs from 'fs-extra';
+import path from 'path';
 import { BundleAnalyzer } from '@/components/BundleAnalyzer/BundleAnalyzer';
-import { useMemo } from 'react';
 
-export default function AppAnalyze() {
-  const options = useMemo(
-    () => [
-      {
-        value: 'client',
-        label: 'Browser',
-      },
-      {
-        value: 'nodejs',
-        label: 'Node.js',
-      },
-      { value: 'edge', label: 'Edge' },
-    ],
-    [],
-  );
+async function getRuntimes() {
+  const inItStatsPath = path.join(process.cwd(), '.next/in-it-stats');
+  const files = await fs.readdir(inItStatsPath);
+  const runtimes = files.map((file) => {
+    return path.basename(file, '.json');
+  });
+  return runtimes;
+}
+
+export default async function AppAnalyze() {
+  const runtimes = await getRuntimes();
+  const options = runtimes.map((runtime) => ({
+    value: runtime,
+    label: `${runtime.charAt(0).toUpperCase()}${runtime.slice(1)}`,
+  }));
+
   return (
     <div className="flex flex-col gap-2">
       <BundleAnalyzer options={options} />
