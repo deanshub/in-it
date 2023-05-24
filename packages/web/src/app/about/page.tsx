64e75573e59c4d1df5657c2fb41b0752dd333d21
Dean Shub <deanshub@gmail.com>
2023-05-24 10:41:32 +0300

    about page

---
+++ packages/web/src/app/about/page.tsx
@@ -0,0 +1,9 @@
+import About from './about.mdx';
+
+export default function AboutPage() {
+  return (
+    <div className="prose dark:prose-dark pt-10 pb-14 px-24 leading-loose">
+      <About />
+    </div>
+  )
+}
