c37f224fef631b069bb1c78154356544ab8c9c4f
Amit Shalev <amishtalev@gmail.com>
2023-05-30 17:03:58 +0300

    Revert "fix get stats api"
    This reverts commit 401a7acb1e71679c148e8e1f7daf00c94f524c60.

--- packages/web/src/app/analyze/[appId]/[statsId]/page.tsx
+++ packages/web/src/app/analyze/[appId]/[statsId]/page.tsx
@@ -1,10 +1,8 @@
 import { BundleAnalyzer } from '@/components/BundleAnalyzer/BundleAnalyzer';
-import dbConnect from '@/db/dbConnect';
 import { Stats } from '@/db/models';
 import Mongoose from 'mongoose';
 
 async function getStats(appId: string, statsId: string) {
-  await dbConnect();
   const stats = await Stats.findById(new Mongoose.Types.ObjectId(statsId));
   return stats;
 }
