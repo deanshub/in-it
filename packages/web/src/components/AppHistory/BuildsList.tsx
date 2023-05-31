b603e1309637cb513fd2b99b3883b0d7e0dcb967
Dean Shub <deanshub@gmail.com>
2023-05-31 21:17:17 +0300

    table actions

--- packages/web/src/components/AppHistory/BuildsList.tsx
+++ packages/web/src/components/AppHistory/BuildsList.tsx
@@ -1,12 +1,11 @@
 import filesize from 'filesize';
 import { formatDistanceToNow } from 'date-fns';
 import { AiOutlineDeploymentUnit, AiOutlineDiff } from 'react-icons/ai';
-import { Button } from '../basic/button';
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../basic/table';
 import { SiGithub } from 'react-icons/si';
 import { BiFileFind } from 'react-icons/bi';
 import { PagingFooter } from './PagingFooter';
-import Link from 'next/link';
+import { TableAction } from './TableAction';
 import type { BuildItemType } from '@/db/queries';
 
 interface BuildsListProps {
@@ -73,27 +72,15 @@ function BuildItem({
       <TableCell className="py-1">{filesize(parsedSize)}</TableCell>
       <TableCell className="py-1 text-right">
         {repository && commitHash ? (
-          <Link
-            target="_blank"
+          <TableAction
+            tooltip="View on GitHub"
             href={`${providerHost}/${repository}/commit/${commitHash}`}
-            prefetch={false}
-          >
-            <Button variant="ghost" size="sm">
-              <SiGithub />
-            </Button>
-          </Link>
+            icon={SiGithub}
+          />
         ) : null}
-        <Button variant="ghost" size="sm">
-          <AiOutlineDeploymentUnit />
-        </Button>
-        <Button variant="ghost" size="sm">
-          <AiOutlineDiff />
-        </Button>
-        <Link href={`/analyze/${appId}/${_id}`} prefetch={false}>
-          <Button variant="ghost" size="sm">
-            <BiFileFind />
-          </Button>
-        </Link>
+        <TableAction disabled tooltip="Dependency Graph" icon={AiOutlineDeploymentUnit} />
+        <TableAction disabled tooltip="Bundle Diff" icon={AiOutlineDiff} />
+        <TableAction tooltip="Analyze Bundle" href={`/analyze/${appId}/${_id}`} icon={BiFileFind} />
       </TableCell>
     </TableRow>
   );
---
+++ packages/web/src/components/AppHistory/TableAction.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+import { Button } from '../basic/button';
+import { TooltipProvider, Tooltip, TooltipTrigger, TooltipContent } from '../basic/tooltip';
+import type { IconType } from 'react-icons';
+
+interface TableActionProps {
+  href?: string;
+  icon: IconType;
+  tooltip: React.ReactNode;
+  disabled?: boolean;
+}
+export function TableAction({ href, icon: Icon, tooltip, disabled }: TableActionProps) {
+  return (
+    <TableActionWrapper tooltip={tooltip} href={href}>
+      <Button variant="ghost" size="sm" disabled={disabled}>
+        <Icon />
+      </Button>
+    </TableActionWrapper>
+  );
+}
+interface TableActionWrapperProps {
+  children: React.ReactNode;
+  href?: string;
+  tooltip: React.ReactNode;
+}
+function TableActionWrapper({ children, href, tooltip }: TableActionWrapperProps) {
+  return (
+    <TooltipProvider delayDuration={500}>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          {href ? (
+            <Link
+              href={href}
+              prefetch={false}
+              target={href.startsWith('http') ? '_blank' : undefined}
+            >
+              {children}
+            </Link>
+          ) : (
+            children
+          )}
+        </TooltipTrigger>
+        <TooltipContent>{tooltip}</TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+}
