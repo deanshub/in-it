bc93621fea48b4bc39c6037f54d6d7d445e36a36
Dean Shub <deanshub@gmail.com>
2023-05-31 14:19:30 +0300

    fix build

--- packages/web/src/utils/getAppId.ts
+++ packages/web/src/utils/getAppId.ts
@@ -19,32 +19,32 @@ export async function getAppId({
     });
 
     if (apps.length === 1) {
-      return apps[0]._id;
+      return apps[0]._id.toString();
     }
     if (apps.length > 1) {
       let appsWithPackagePath = apps.filter((a) => a.packagePath === packagePath);
       if (appsWithPackagePath.length === 1) {
-        return appsWithPackagePath[0]._id;
+        return appsWithPackagePath[0]._id.toString();
       } else if (appsWithPackagePath.length === 0) {
         appsWithPackagePath = apps;
       }
 
       let appsWithName = appsWithPackagePath.filter((a) => a.name === name);
       if (appsWithName.length === 1) {
-        return appsWithName[0]._id;
+        return appsWithName[0]._id.toString();
       } else if (appsWithName.length === 0) {
         appsWithName = appsWithPackagePath;
       }
 
       let appsWithPackageName = appsWithName.filter((a) => a.packageName === packageName);
       if (appsWithPackageName.length === 1) {
-        return appsWithPackageName[0]._id;
+        return appsWithPackageName[0]._id.toString();
       } else if (appsWithPackageName.length === 0) {
         appsWithPackageName = appsWithName;
       }
 
       if (appsWithPackageName.length === 1) {
-        return appsWithPackageName[0]._id;
+        return appsWithPackageName[0]._id.toString();
       }
     }
   }
