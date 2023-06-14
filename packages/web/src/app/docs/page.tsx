4613d81a064554fa7f628934076c95bf8e450463
Dean Shub <deanshub@gmail.com>
2023-06-14 11:08:48 +0300

    responsive

--- packages/web/src/app/docs/page.tsx
+++ packages/web/src/app/docs/page.tsx
@@ -1,7 +1,7 @@
 import Docs from './docs.mdx';
 export default function DocPage() {
   return (
-    <div className="prose dark:prose-dark pb-14 px-40 leading-loose">
+    <div className="prose dark:prose-dark pb-14 px-5 lg:px-40 leading-loose">
       <Docs />
     </div>
   );
